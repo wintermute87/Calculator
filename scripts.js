f5373629ab98425fe567bc383d2f2db81cc73c81
wintermute87 <drimba.adrian@gmail.com>
2023-03-06 09:10:10 +0200

    Create 'operate' function

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 
 function add(a,b)
 {
-    return a+b;
+    return (a+b).toString();
 }
 
 
 function subtract(a,b)
 {
-    return a-b;
+    return (a-b).toString();
 }
 
 
 function multiply(a,b)
 {
-    return a*b;
+    return (a*b).toString();
 }
 
 function divide(a,b)
@@ -21,5 +21,34 @@ function divide(a,b)
     if(b==0)
         return "NaN"
     else
-        return a/b;
+        return (a/b).toString();
+}
+
+function operate(operationType, a,b)
+{
+    let result;
+    switch (operationType) {
+        case "add":
+            result = add(a,b)
+            break;
+    
+        case "subtract":
+            result = subtract(a,b)
+            break;
+
+        case "multiply":
+            result = multiply(a,b)
+            break;
+
+        case "divide":
+            result = divide(a,b)
+            break;
+
+        default:
+            result = "Wrong operation"
+            break;
+    }
+
+    return result;
+
 }
